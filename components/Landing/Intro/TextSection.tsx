c3a84463b54b77343e327f4a6ee53037681c869f
jknabe1 <101703981+jknabe1@users.noreply.github.com>
2025-05-06 11:54:15 +0200

    Refactor TextSection component for improved styling and structure

--- components/Landing/Intro/TextSection.tsx
+++ components/Landing/Intro/TextSection.tsx
@@ -5,13 +5,13 @@ const TextSection = () => {
     <div>
         <section className="flex flex-col gap-8 overflow-hidden lg:gap-32 lg:grid lg:grid-flow-col">
             <div className="col-span-2">
-                <p className="text-3xl leading-tight 2xl:text-7xl lg:text-6xl">Huset ligger i världens mysigaste plats - Wadköping. Precis bakom spelgårdens läktare i närheten av teaterladan, granne med stadsparken. Örebros nya sommarpärla. Öppnar sommar 2025. Mackor, Musik & Gemenskap i Wadköping. </p>
+                <p className="kicker">Huset ligger i världens mysigaste plats - Wadköping. Precis bakom spelgårdens läktare i närheten av teaterladan, granne med stadsparken. Örebros nya sommarpärla. Öppnar sommar 2025. Mackor, Musik & Gemenskap i Wadköping. </p>
             </div>
             <div className="col-span-1 text-4xl">
                 <p>Öppettider:</p>
                 <p>Mån - Fre: 11-18</p>
                 <p>Lör - Sön: 10-16</p>
-                <div className="pt-8 -mb-24 -mr-10"></div>
+                <div className="pt-8"></div>
             </div>
           </section>
     </div>
